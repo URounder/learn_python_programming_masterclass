4df034041da33720a5d7ea53be379a605f6171ca
URounder <eric.graul@gmail.com>
2023-01-26 15:25:31 -0500

    demo and song added

--- object_oriented_python/accounts.py
+++ object_oriented_python/accounts.py
@@ -1,57 +1,70 @@
+import datetime
+import pytz
+
+
 class Account:
     """Simple account class with balance"""
 
+    @staticmethod
+    def _current_time():
+        utc_time = datetime.datetime.utcnow()
+        return pytz.utc.localize(utc_time)
+
     def __init__(self, name, balance):
-        self.name = name
-        self.balance = balance
-        print("Account created for " + self.name)
+        self._name = name
+        self.__balance = balance
+        self._transaction_list = []
+        if balance > 0:
+            self._transaction_list.append((Account._current_time(), balance))
+        print("Account created for " + self._name)
+        self.show_balance()
 
     def deposit(self, amount):
         if amount > 0:
-            self.balance += amount
+            self.__balance += amount
             self.show_balance()
+            self._transaction_list.append((Account._current_time(), amount))
 
     def withdraw(self, amount):
-        if 0 < amount <= self.balance:
-            self.balance -= amount
+        if 0 < amount <= self.__balance:
+            self.__balance -= amount
             self.show_balance()
+            self._transaction_list.append((Account._current_time(), -amount))
         else:
             print(
                 "The amount must be greater than zero and no more than your account balance"
             )
 
     def show_balance(self):
-        print("Balance is {}".format(self.balance))
+        print("Balance is {}".format(self.__balance))
+
+    def show_transactions(self):
+        for date, amount in self._transaction_list:
+            if amount > 0:
+                tran_type = "deposited"
+            else:
+                tran_type = "withdrawn"
+                amount *= -1
+            print(
+                "{:6} {} on {} (local time was {})".format(
+                    amount, tran_type, date, date.astimezone()
+                )
+            )
 
 
 if __name__ == "__main__":
     tim = Account("Tim", 0)
-    tim.show_balance()
 
     tim.deposit(1000)
     tim.withdraw(500)
     tim.withdraw(2000)
-
-    print(tim)
-    print(str(tim))
-    print(repr(tim))
-    print(tim.__str__())
-    print(tim.__repr__())
+    tim.show_transactions()
 
     steph = Account("Steph", 800)
+    steph.__balance = 200
     steph.deposit(100)
     steph.withdraw(200)
-    steph.show_balance()
-
+    steph.show_transactions()
     print(steph.__dict__)
-    steph.balance = 40
+    steph._Account__balance = 40
     steph.show_balance()
-    print(steph.__dict__)
-
-    steph._balance = 100
-    steph.show_balance()
-    print(steph.__dict__)
-
-    steph.__balance = -100
-    steph.show_balance()
-    print(steph.__dict__)
